3ed835dbb76a7134d7c61cd47a6714adcc62c8aa
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-08-23 10:17:16 -0600

    tab built out along with routes, MVP reqs met

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import LocationsList from "./components/LocationsList";
 import EpisodeList from "./components/EpisodeList";
+import WelcomePage from "./components/WelcomePage";
+import { Route } from "react-router-dom";
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
-      <LocationsList />
-      <EpisodeList />
-      {/* <TabNav /> */}
+      <TabNav />
+      <Route path="/home" component = {WelcomePage} />
+      <Route path ="/character" component = {CharacterList} />
+      <Route path = "/location" component = {LocationsList} />
+      <Route path = "/episode" component = {EpisodeList} />
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+import { Tab } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import CharacterList from "./CharacterList";
+import LocationsList from "./LocationsList";
+import EpisodeList from "./EpisodeList";
+import WelcomePage from "./WelcomePage";
 
-// TODO: Add missing tabs below
-// Take a look at React Semantic UI tabs 
-// https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
+const panes = [
+    { menuItem: 'Home', render: () => <WelcomePage /> },
+    { menuItem: 'Characters', render: () => <CharacterList /> },
+    { menuItem: 'Locations', render: () => <LocationsList /> },
+    { menuItem: 'Episodes', render: () => <EpisodeList /> },
+  ]
+  
+  const TabNav = () => <Tab panes={panes} />
+  
+  export default TabNav;
 
-};
 
